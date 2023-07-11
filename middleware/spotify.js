2be6876af253302f3a80d2b6119972b6b8d812cc
Darragh Murphy <dmurphy@servuo.com>
2023-07-10 20:39:47 -0700

    Move logic to middleware

--- middleware/spotify.js
+++ middleware/spotify.js
@@ -57,4 +57,86 @@ async function getSpotifyClient(req, res, next) {
   }
 }
 
-module.exports = { spotifyApiContainer, getSpotifyClient };
+spotifyApiContainer.createApiInstance('login');
+const spotifyApi = spotifyApiContainer.getApiInstance('login');
+
+const getTokens = async (req, res, next) => {
+  const { error, code } = req.query;
+  if (error) {
+    console.error('Callback Error:', error);
+    return res.send(`Callback Error: ${error}`);
+  }
+
+  try {
+    const data = await spotifyApi.authorizationCodeGrant(code);
+    const { access_token, refresh_token, expires_in } = data.body;
+
+    spotifyApi.setAccessToken(access_token);
+    spotifyApi.setRefreshToken(refresh_token);
+
+    req.tokens = {
+      access_token,
+      refresh_token,
+      expires_in,
+    };
+    next();
+  } catch (err) {
+    console.error('Error getting Tokens:', err);
+    return res.send(`Error getting Tokens: ${err}`);
+  }
+};
+
+const getUserData = async (req, res, next) => {
+  try {
+    const user = await spotifyApi.getMe();
+    const userId = user.body.id;
+
+    const updatedUser = await prisma.user.upsert({
+      where: { name: userId },
+      update: {
+        spotify_token: req.tokens.access_token,
+        spotify_refresh_token: req.tokens.refresh_token,
+      },
+      create: {
+        name: userId,
+        spotify_token: req.tokens.access_token,
+        spotify_refresh_token: req.tokens.refresh_token,
+      },
+    });
+
+    req.userId = userId;
+    req.updatedUser = updatedUser;
+    next();
+  } catch (err) {
+    console.error('Error getting user ID:', err);
+    return res.send(`Error getting user ID: ${err}`);
+  }
+};
+
+const createPlaylist = async (req, res, next) => {
+  try {
+    const data = await spotifyApi.createPlaylist(`NextUp - ${req.userId}`, {
+      description: 'nextup.rocks',
+      public: false,
+    });
+
+    let playListId = data.body['id'];
+
+    const playlist = await prisma.playlist.create({
+      data: { spotify_id: playListId },
+    });
+    const event = await prisma.event.create({
+      data: {
+        host: { connect: { id: req.updatedUser.id } },
+        playlist: { connect: { id: playlist.id } },
+      },
+    });
+
+    next();
+  } catch (err) {
+    console.error('Error creating playlist:', err);
+    return res.send(`Error creating playlist: ${err}`);
+  }
+};
+
+module.exports = { spotifyApiContainer, getSpotifyClient, createPlaylist, getUserData, getTokens };
