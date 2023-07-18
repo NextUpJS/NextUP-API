f9c4360cf51474631ae3eddc321748da8c9a6bdd
Darragh Murphy <dmurphy@servuo.com>
2023-07-18 13:25:35 -0700

    revert cron to earlier version.

--- scheduledFunctions/cron.js
+++ scheduledFunctions/cron.js
@@ -10,11 +10,7 @@ exports.initScheduledJobs = () => {
       include: {
         playlist: {
           include: {
-            queue: {
-              orderBy: {
-                position: 'asc',
-              },
-            },
+            queue: true,
           },
         },
         host: true,
@@ -53,8 +49,74 @@ exports.initScheduledJobs = () => {
       if (track) {
         try {
           const currentEvent = await prisma.event.findUnique({ where: { id: event.id } });
+          // Update the currently playing track in the event
 
-          // Additional logic and database interactions here...
+          // Check if the track and artist exist in the database
+          let trackInDb = await prisma.track.findUnique({ where: { id: track.id } });
+          let artistInDb = await prisma.artist.findUnique({ where: { id: track.artists[0].id } });
+          let albumInDb = await prisma.album.findUnique({ where: { id: track.album.id } });
+
+          // If the artist doesn't exist, add it
+          if (!artistInDb && track.artists[0]) {
+            artistInDb = await prisma.artist.create({
+              data: {
+                id: track.artists[0].id,
+                name: track.artists[0].name,
+                href: track.artists[0].href,
+                spotifyUrl: track.artists[0].external_urls.spotify,
+                uri: track.artists[0].uri,
+                artistType: track.artists[0].type,
+              },
+            });
+            console.log(`Added artist '${artistInDb.name}' to the database.`);
+          }
+
+          if (!albumInDb) {
+            albumInDb = await prisma.album.create({
+              data: {
+                id: track.album.id,
+                albumType: track.album.album_type,
+                externalSpotifyUrl: track.album.external_urls.spotify,
+                href: track.album.href,
+                name: track.album.name,
+                releaseDate: track.album.release_date,
+                releaseDatePrecision: track.album.release_date_precision,
+                totalTracks: track.album.total_tracks,
+                uri: track.album.uri,
+                // Add other album properties if necessary
+                Artist: {
+                  connect: { id: track.artists[0].id }, // Connect the album to its artist
+                },
+              },
+            });
+            console.log(`Added album '${albumInDb.name}' to the database.`);
+          }
+
+          // If the track doesn't exist, add it
+          if (!trackInDb) {
+            trackInDb = await prisma.track.create({
+              data: {
+                id: track.id,
+                name: track.name,
+                discNumber: track.disc_number,
+                durationMs: track.duration_ms,
+                explicit: track.explicit,
+                isrc: track.external_ids.isrc,
+                externalUrl: track.external_urls.spotify,
+                href: track.href,
+                isLocal: track.is_local,
+                popularity: track.popularity,
+                previewUrl: track.preview_url,
+                trackNumber: track.track_number,
+                trackType: track.type,
+                uri: track.uri,
+                albumId: track.album.id,
+                artistId: track.artists[0].id,
+                // Add other track properties if necessary
+              },
+            });
+            console.log(`Added track '${trackInDb.name}' to the database.`);
+          }
 
           if (currentEvent.playingTrackId !== track.id) {
             await prisma.event.update({
@@ -67,6 +129,8 @@ exports.initScheduledJobs = () => {
           }
         } catch (error) {
           console.error('Error occurred:', error);
+          // You can log the error to a file or a centralized logging system
+          // for better error tracking and debugging.
         }
       }
 
@@ -78,45 +142,26 @@ exports.initScheduledJobs = () => {
       ) {
         console.log(`The track '${track.name}' has ended for event: ${event.id}`);
 
-        const validQueueItems = event.playlist.queue.filter((item) => item.position >= 0);
-        if (validQueueItems.length > 0) {
-          const nextSong = validQueueItems[0];
+        // if the queue is not empty, play the next song in the queue
+        if (event.playlist.queue.length > 0) {
+          const nextSong = event.playlist.queue[0]; // assumes the queue is a list and the next song is at index 0
           console.log(nextSong);
           await spotifyClient.play({ uris: [`spotify:track:${nextSong.trackId}`] });
 
+          // remove the song from the queue
           await prisma.playlist.update({
             where: { id: event.playlist.id },
             data: {
               queue: {
-                update: {
-                  where: { id: nextSong.id },
-                  data: { position: -1 },
-                },
+                delete: { id: nextSong.id },
               },
             },
           });
           console.log(`Played next song`);
-
-          // Decrement position of the rest of the queue
-          for (let i = 1; i < validQueueItems.length; i++) {
-            const queueItem = validQueueItems[i];
-
-            await prisma.playlist.update({
-              where: { id: event.playlist.id },
-              data: {
-                queue: {
-                  update: {
-                    where: { id: queueItem.id },
-                    data: { position: queueItem.position - 1 },
-                  },
-                },
-              },
-            });
-          }
-          console.log('Updated song positions in the queue');
         } else {
           console.log(`The queue is empty, no song to play.`);
 
+          // Get recommendations based on the last played song
           const recommendations = await spotifyClient.getRecommendations({
             seed_tracks: [track.id],
             min_energy: 0.4,
@@ -125,18 +170,23 @@ exports.initScheduledJobs = () => {
           });
 
           if (recommendations.body.tracks.length > 0) {
+            // Select a random track from the recommendations
             const randomIndex = Math.floor(Math.random() * recommendations.body.tracks.length);
             const randomTrack = recommendations.body.tracks[randomIndex];
 
+            // Retrieve the last_random_song_attempt for the event
             const lastRandomSongAttempt = new Date(event.last_random_song_attempt);
             const now = new Date();
 
+            // Check if at least 5 seconds have passed since the last attempt
             if (now.getTime() - lastRandomSongAttempt.getTime() >= 5000) {
+              // Play the selected random track
               await spotifyClient.play({ uris: [`spotify:track:${randomTrack.id}`] });
               console.log(
                 `Played a random recommended song: '${randomTrack.name}' by '${randomTrack.artists[0].name}'`,
               );
 
+              // Update the last_random_song_attempt in the event
               await prisma.event.update({
                 where: { id: event.id },
                 data: { last_random_song_attempt: now },
