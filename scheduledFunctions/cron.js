e74798b0a2925b740d8b7e2857cf55351c5f37b6
Darragh Murphy <dmurphy@servuo.com>
2023-07-18 19:56:18 -0700

    Fix ordering

--- scheduledFunctions/cron.js
+++ scheduledFunctions/cron.js
@@ -143,26 +143,28 @@ exports.initScheduledJobs = () => {
         console.log(`The track '${track.name}' has ended for event: ${event.id}`);
 
         // if the queue is not empty, play the next song in the queue
+        // if the queue is not empty, play the next song in the queue
         const songsWithPosition = event.playlist.queue.filter((song) => song.position > 0);
         if (songsWithPosition.length > 0) {
           // sort the queue by position in ascending order
           songsWithPosition.sort((a, b) => a.position - b.position);
 
-          // check if the queue positions are sequential
-          let isSequential = true;
+          // check if the queue positions are sequential and start at 1
+          let isSequentialAndStartsAtOne = songsWithPosition[0].position === 1;
           for (let i = 0; i < songsWithPosition.length - 1; i++) {
             if (songsWithPosition[i].position + 1 !== songsWithPosition[i + 1].position) {
-              isSequential = false;
+              isSequentialAndStartsAtOne = false;
               break;
             }
           }
 
-          if (!isSequential) {
+          // if positions are not sequential and starting at 1, update them
+          if (!isSequentialAndStartsAtOne) {
             for (let i = 0; i < songsWithPosition.length; i++) {
               await prisma.queue.update({
                 where: { id: songsWithPosition[i].id },
                 data: {
-                  position: i + 1,
+                  position: i + 1, // start at 1 and increment from there
                 },
               });
             }
@@ -181,6 +183,7 @@ exports.initScheduledJobs = () => {
             console.log(`Played next song`);
           }
 
+          // decrement positions of all songs in the queue
           for (const song of event.playlist.queue) {
             await prisma.queue.update({
               where: { id: song.id },
